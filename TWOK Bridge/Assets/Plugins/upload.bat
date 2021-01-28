20e2cc6987433959ed0541d6257e172b9893fd86
ISpeakKlingon <77762797+ISpeakKlingon@users.noreply.github.com>
2021-01-27 22:04:14 -0800

    Green disc added

--- TWOK Bridge/Assets/Plugins/upload.bat
+++ TWOK Bridge/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@F:\Unity Project Media\AltSpace\Testing\TWOK Bridge\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1645433754543456303.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@F:\Unity Project Media\GitHub Repository\TWOK-Bridge\TWOK Bridge\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1645433754543456303.json
